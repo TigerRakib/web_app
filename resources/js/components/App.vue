1f9530e65b96db2b71603e4e943aad287dda3586
tiger <nazrulislamrakib1813030@gmail.com>
2025-06-28 00:42:07 +0600

    Install and build Vue.js admin panel

---
+++ resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <LoginForm v-if="!token" @login="setToken" />
+    <Dashboard v-else :token="token" />
+  </div>
+</template>
+
+<script>
+import LoginForm from './LoginForm.vue';
+import Dashboard from './Dashboard.vue';
+
+export default {
+  components: { LoginForm, Dashboard },
+  data() {
+    return { token: null };
+  },
+  methods: {
+    setToken(token) {
+      this.token = token;
+    }
+  }
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
+import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel(['resources/js/app.js']),
+        vue(),
     ],
 });

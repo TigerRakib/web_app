a745eb7a332c731bc329e87bba7c44d068bc0e79
tiger <nazrulislamrakib1813030@gmail.com>
2025-06-28 00:04:04 +0600

    Create product API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,4 +17,9 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/products', [ProductController::class, 'index']); // Public endpoint
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('products', ProductController::class)->except(['index']);
 });
